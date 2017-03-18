58147cb75ddea1c4ce2b8802489d36a3b8f5c08e
Sty <styboulits@gmail.com>
2017-03-18 13:15:40 +0200

    Began implementation of HaxController.

---
+++ src/HaxAPI/Controllers/HaxController.cs
@@ -0,0 +1,23 @@
+﻿namespace HaxAPI.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using HaxLib;
+    using HaxLib.Models;
+
+    [Route("api/Hax")]
+    public class HaxController : Controller
+    {
+        private IDatabase database;
+
+        public HaxController(IDatabase database)
+        {
+            this.database = database;
+        }
+
+
+    }
+}
