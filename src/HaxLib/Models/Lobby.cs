5dac98cfa08b882eca55d1faeb9f501b3b6ed7d7
Sty <styboulits@gmail.com>
2017-03-18 10:25:26 +0200

    Implemented Lobby and User models.

---
+++ src/HaxLib/Models/Lobby.cs
@@ -0,0 +1,29 @@
+﻿namespace HaxLib.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class Lobby
+    {
+        public int ID { get; private set; }
+        public string State { get; private set; }
+        public int HostID { get; private set; }
+        public decimal? TotalPayAmount { get; private set; }
+        public string ReceiptUrl { get; private set; }
+        public List<User> Members { get; private set; }
+
+        public Lobby(int id, string state, int hostid) : this(id, state, hostid, null, null, new List<User>())
+        {
+        }
+
+        public Lobby(int id, string state, int hostid, decimal? totalpayamount, string receipturl, List<User> members)
+        {
+            this.ID = id;
+            this.State = state;
+            this.HostID = hostid;
+            this.TotalPayAmount = totalpayamount;
+            this.ReceiptUrl = receipturl;
+            this.Members = members;
+        }
+    }
+}
---
+++ src/HaxLib/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace HaxLib.Models
+{
+    public sealed class User
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public bool Verified { get; private set; }
+        public int? LobbyID { get; private set; }
+        public decimal? PayAmount { get; private set; }
+
+        public User(int id, string name, bool verified, int? lobbyid, decimal? payamount)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Verified = verified;
+            this.LobbyID = lobbyid;
+            this.PayAmount = payamount;
+        }
+    }
+}
