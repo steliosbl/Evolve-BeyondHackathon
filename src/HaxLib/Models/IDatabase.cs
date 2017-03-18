844e9a72bc3759f7605b86390dd0172f62a77366
Sty <styboulits@gmail.com>
2017-03-18 10:42:32 +0200

    Implemented IDatabase.

---
+++ src/HaxLib/Models/IDatabase.cs
@@ -0,0 +1,12 @@
+﻿namespace HaxLib.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IDatabase
+    {
+        User GetUser(int id);
+        Lobby GetLobby(int id);
+        List<User> GetLobbyMembers(int id);
+    }
+}
